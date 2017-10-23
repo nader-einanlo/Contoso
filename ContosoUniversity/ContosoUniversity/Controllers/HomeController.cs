b645cd34762250f88231ec1788b152b6e0eae2ae
Nader Einanlo <SE2321000131-P000000241660@vgregion.se>
2017-10-24 00:05:01 +0200

    Contoso University

---
+++ ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/SchoolViewModels/AssignedCourseData.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ContosoUniversity.SchoolViewModels
+{
+    public class AssignedCourseData
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
